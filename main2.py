4334c561c076a94e24cc69cd4df2fb492f9d7ac6
Dekku31 <volkswagengolf731@gmail.com>
2023-02-08 09:21:29 +0100

    main2.py elkészült

--- main2.py
+++ main2.py
@@ -5,20 +5,24 @@ print("Tanulok feldolgozása.")
 tanulok = []
 while True:
     print("\nKérem a tanuló adatait:")
-    nev = input("Tanuló")
+    nev = input("Tanuló neve: ")
     szId = input("Születési ideje:")
     magassag = int(input("Magasság: "))
 
     tanulo =(nev, szId, magassag)
     tanulok.append(tanulo)
 
-    valasz = input("További tanuli (igen/nem): ")
+    valasz = input("További tanulo (igen/nem): ")
     if valasz.lower() != 'igen':
         break
+
 #2
 
 
-#3 Hozzáférés 
+#3 Hozzáférés listaelem index segítségével
 print("\nTanulók listája\n")
-for item in tanulok:
-    print(f"Név: {item[0]}, születési ideje: {item[1]}, magasság: {item[2]}")
+i=0
+while i<len(tanulok):
+    print(f"Sorszám {i+1},Név: {tanulok[i][0]}, születési ideje: {tanulok[i][1]}, magasság: {tanulok[i][2]}")
+
+    i+=1
